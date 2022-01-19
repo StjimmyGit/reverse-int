110ace8d13aec88a85dcef07b1433fbd934e6d23
Ilya <admin@vodokanal.local>
2022-01-19 08:16:04 +0300

    init: create reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    let result = n.toString().split('').reverse().join('');
+
+    if (n > 0) {
+        return result;
+
+    } else {
+        return -result;
+    }
 }
